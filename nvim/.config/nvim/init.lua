6fb164dd5f86c145214c3c3abeb03a2d51e3712f
Jeet Patel <jp707049@gmail.com>
2022-10-09 22:15:58 -0500

    added packer,colorscheme and some keymaps to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,4 @@
 require("jp.options")
+require("jp.packer")
+require("jp.colorscheme")
+require("jp.keymaps")
---
+++ nvim/.config/nvim/lua/jp/packer.lua
@@ -0,0 +1,8 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use "wbthomason/packer.nvim"
+
+  -- Colorscheme
+  use "gruvbox-community/gruvbox"
+
+end)
