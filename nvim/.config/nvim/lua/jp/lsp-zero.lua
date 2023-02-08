08356c1104d0bd69b989cbd9a3d81ee7a775587c
Jeet Patel <jeet_patel3@apple.com>
2023-02-07 21:40:10 -0600

    only use nvim_lspand path  cmp source

--- nvim/.config/nvim/lua/jp/lsp-zero.lua
+++ nvim/.config/nvim/lua/jp/lsp-zero.lua
@@ -1,4 +1,17 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+local cmp = require('cmp')
+
+local cmp_config = lsp.defaults.cmp_config({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+  },
+  window = {
+    completion = cmp.config.window.bordered()
+  },
+})
+
+cmp.setup(cmp_config)
 lsp.setup()
