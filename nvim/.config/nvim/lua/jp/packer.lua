f2b6a4b1afd5745f59e53bb966b7841c313cd948
Jeet Patel <jeet_patel3@apple.com>
2023-02-14 14:36:15 -0600

    fix

--- nvim/.config/nvim/lua/jp/packer.lua
+++ nvim/.config/nvim/lua/jp/packer.lua
@@ -39,14 +39,14 @@ return require("packer").startup(function(use)
       {"hrsh7th/nvim-cmp"},
       {"hrsh7th/cmp-buffer"},
       {"hrsh7th/cmp-path"},
-      --{"saadparwaiz1/cmp_luasnip"},
+      {"saadparwaiz1/cmp_luasnip"},
       {"hrsh7th/cmp-nvim-lsp"},
       {"hrsh7th/cmp-nvim-lua"},
 
       -- Snippets
-      --{"L3MON4D3/LuaSnip"},
+      {"L3MON4D3/LuaSnip"},
       -- Snippet Collection (Optional)
-      --{"rafamadriz/friendly-snippets"},
+      {"rafamadriz/friendly-snippets"},
     }
   }
 
