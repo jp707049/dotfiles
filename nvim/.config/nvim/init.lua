182e7af75bc38149f74a9fc39eb2806892115e24
Jeet Patel <jeet_patel3@apple.com>
2023-06-10 14:31:56 -0500

    back to nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Import Lua modules
+require('core/lazy')
+require('core/autocmds')
+require('core/keymaps')
+require('core/statusline')
+require('core/options')
+require('lsp/lspconfig')
+require('plugins/indent-blankline')
+require('plugins/nvim-cmp')
+require('plugins/nvim-treesitter')
---
+++ nvim/.config/nvim/lua/core/lazy.lua
@@ -0,0 +1,99 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Use a protected call so we don't error out on first use
+local status_ok, lazy = pcall(require, 'lazy')
+if not status_ok then
+  return
+end
+
+-- Start setup
+lazy.setup({
+  spec = {
+    -- Colorschemes
+    { 'RRethy/nvim-base16',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd('colorscheme base16-default-dark')
+      end
+    },
+
+    -- Icons
+    { 'kyazdani42/nvim-web-devicons', lazy = true },
+
+    -- Git labels
+    {
+      'lewis6991/gitsigns.nvim',
+      lazy = true,
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons',
+      },
+      config = function()
+        require('gitsigns').setup{}
+      end
+    },
+
+    -- Statusline
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = {
+        'kyazdani42/nvim-web-devicons',
+      },
+    },
+
+    -- Treesitter
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+
+    -- Indent line
+    { 'lukas-reineke/indent-blankline.nvim' },
+
+    -- Autopair
+    {
+      'windwp/nvim-autopairs',
+      event = 'InsertEnter',
+      config = function()
+        require('nvim-autopairs').setup{}
+      end
+    },
+
+    -- LSP
+    { 'neovim/nvim-lspconfig' },
+
+    -- Autocomplete
+    {
+      'hrsh7th/nvim-cmp',
+      -- load cmp on InsertEnter
+      event = 'InsertEnter',
+      -- these dependencies will only be loaded when cmp loads
+      -- dependencies are always lazy-loaded unless specified otherwise
+      dependencies = {
+        'L3MON4D3/LuaSnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
+        'saadparwaiz1/cmp_luasnip',
+      },
+    },
+
+    -- LSP Status
+    {
+      'j-hui/fidget.nvim',
+      config = function()
+        require('fidget').setup{}
+      end
+    },
+  },
+})
