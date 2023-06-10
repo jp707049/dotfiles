91e5920aa21aaa7ca8b37d657435501cf2df7906
Jeet Patel <jeet_patel3@apple.com>
2023-06-10 15:07:59 -0500

    add signcolum to nvim

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -24,6 +24,7 @@ opt.linebreak = true        -- Wrap on word boundary
 opt.termguicolors = true    -- Enable 24-bit RGB colors
 opt.laststatus=3            -- Set global statusline
 opt.showmode = false				-- Disable showing mode in command line
+opt.signcolumn = 'yes'
 
 -----------------------------------------------------------
 -- Tabs, indent
