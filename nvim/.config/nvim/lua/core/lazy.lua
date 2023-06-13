2e9d83bbb23ad0c12646ce0877e95526a7b6983f
Jeet Patel <jeet_patel3@apple.com>
2023-06-13 11:11:58 -0500

    use base16 default dar theme

--- nvim/.config/nvim/lua/core/lazy.lua
+++ nvim/.config/nvim/lua/core/lazy.lua
@@ -22,10 +22,11 @@ end
 lazy.setup({
   spec = {
     -- Colorschemes
-    { 'RRethy/nvim-base16',
+    {
+      'RRethy/nvim-base16',
       lazy = false,
       priority = 1000,
-      config = function()
+      config = function ()
         vim.cmd('colorscheme base16-default-dark')
       end
     },
