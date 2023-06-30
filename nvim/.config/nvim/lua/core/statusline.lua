18739fca73e5e2ad45d14ec5cf6c8f316fd8e021
Jeet Patel <jeet_patel3@apple.com>
2023-06-29 21:40:03 -0500

    improve statusline nvim

--- nvim/.config/nvim/lua/core/statusline.lua
+++ nvim/.config/nvim/lua/core/statusline.lua
@@ -2,8 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -19,7 +19,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
