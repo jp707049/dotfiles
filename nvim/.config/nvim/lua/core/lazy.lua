7ec6a2cea87fcebc30fe303f5f26f813a3d5285e
Jeet Patel <jeet_patel3@apple.com>
2023-07-31 21:47:15 -0500

    remove buffer autocomplete

--- nvim/.config/nvim/lua/core/lazy.lua
+++ nvim/.config/nvim/lua/core/lazy.lua
@@ -84,7 +84,6 @@ lazy.setup({
         'L3MON4D3/LuaSnip',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path',
-        'hrsh7th/cmp-buffer',
         'saadparwaiz1/cmp_luasnip',
       },
     },
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -61,6 +61,5 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
-    { name = 'buffer' },
   },
 }
