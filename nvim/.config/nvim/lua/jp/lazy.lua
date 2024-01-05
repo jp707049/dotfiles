0668bed7afd924b06ad1763ed66becce96842013
Jeet Patel <jeet_patel3@apple.com>
2024-01-04 21:16:31 -0600

    add kitty and use diff theme

--- nvim/.config/nvim/lua/jp/lazy.lua
+++ nvim/.config/nvim/lua/jp/lazy.lua
@@ -24,11 +24,12 @@ lazy.setup({
   spec = {
     -- Colorscheme:
     {
-      'RRethy/nvim-base16',
-      lazy = false, -- make sure we load this during startup if it is your main colorscheme
-      priority = 1000, -- make sure to load this before all the other start plugins
+      'catppuccin/nvim',
+      name = "catppuccin",
+      priority = 1000,
+      lazy = false,
       config = function()
-        vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+        vim.cmd.colorscheme 'catppuccin-macchiato'
       end,
     },
 
