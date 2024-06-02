4507e108559bcf776e17b85620f4bf92d58dda0e
Jeet Patel <jeet_patel3@apple.com>
2024-06-02 13:48:11 -0500

    use gruvbox

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
--- return {
--- 	"RRethy/base16-nvim",
--- 	priority = 1000,
--- 	lazy = false,
--- 	config = function()
--- 		vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
--- 	end,
--- }
-
 return {
-	--"nyoom-engineering/oxocarbon.nvim",
-	"scottmckendry/cyberdream.nvim",
-	lazy = false,
+	"RRethy/base16-nvim",
 	priority = 1000,
-	config = function ()
-		vim.cmd([[colorscheme cyberdream]])
+	lazy = false,
+	config = function()
+		vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
 	end,
 }
+
+-- return {
+-- 	--"nyoom-engineering/oxocarbon.nvim",
+-- 	"scottmckendry/cyberdream.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function ()
+-- 		vim.cmd([[colorscheme cyberdream]])
+-- 	end,
+-- }
