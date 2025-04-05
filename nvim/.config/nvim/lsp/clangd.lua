6165f303ed6186eb1c551652e5ec2463acb9e93e
Jeet Patel <jeet_patel3@apple.com>
2025-04-05 00:15:32 -0500

    remove nvim

--- nvim/.config/nvim/lsp/clangd.lua
+++
@@ -1,6 +0,0 @@
-return {
-  cmd = { 'clangd', '--background-index' },
-  root_markers = { '.clangd', '.clang-format', 'compile_commands.json', 'compile_flags.txt' },
-  filetypes = { 'c', 'cpp' },
-  flags = { debounce_text_changes = 20 },
-}
