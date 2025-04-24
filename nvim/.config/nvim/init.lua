76eaac2c628061e7fc7e10b6446520bd9f14f33d
Jeet Patel <jeet_patel3@apple.com>
2025-04-24 11:10:47 -0500

    added more config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
+
+require('config.options')
+require('config.keymaps')
+require('config.autocmds')
 require("config.lazy")
 vim.lsp.enable({"lua_ls", "basedpyright", "ruff"})
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.number = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = false
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+
+vim.opt.confirm = true
