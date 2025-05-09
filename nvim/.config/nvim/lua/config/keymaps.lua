b4371db15d19b86c768aa1e9f6151c4345fc1017
Jeet Patel <jeet_patel3@apple.com>
2025-05-09 00:59:45 -0500

    added fzf-lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- <leader><leader> toggles between buffers
+vim.keymap.set('n', '<leader><leader>', '<c-^>', { desc = 'Toggles between buffers' })
+
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -16,3 +19,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- fzf
+vim.keymap.set('n', '<leader>ff', function() require('fzf-lua').files() end, { desc = 'Fzf Find Files' })
+vim.keymap.set('n', '<leader>fb', function() require('fzf-lua').buffers() end, { desc = 'Fzf Buffers' })
