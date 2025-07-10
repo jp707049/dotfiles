5083b0530e7407d488985caeb27d97b182ddea16
Jeet Patel <jeet_patel3@apple.com>
2025-07-09 23:11:21 -0500

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -29,6 +29,20 @@ return {
         preset = 'minimal',
       }
     end,
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    name = "catppuccin",
+    config = function ()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+
+      vim.cmd([[ colorscheme catppuccin]])
+    end,
     enabled = true,
   }
 }
