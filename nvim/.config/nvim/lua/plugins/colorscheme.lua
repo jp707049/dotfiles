5dce8e313b179de05a0fbf18606261625795993e
Jeet Patel <jeet_patel3@apple.com>
2025-07-27 14:11:46 -0500

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,52 +9,21 @@
 
 return {
   {
-    "EdenEast/nightfox.nvim",
+    "chriskempson/vim-tomorrow-theme",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme carbonfox]])
-    end,
-    enabled = false,
-  },
-  {
-    "jesseleite/nvim-noirbuddy",
-    dependencies = {
-      { 'tjdevries/colorbuddy.nvim' }
-    },
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('noirbuddy').setup {
-        preset = 'minimal',
-      }
-    end,
-    enabled = false,
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    name = "catppuccin",
-    config = function ()
-      require("catppuccin").setup({
-        transparent_background = true,
-      })
-
-      vim.cmd([[ colorscheme catppuccin]])
-    end,
-    enabled = false,
-  },
-  {
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('onedark').setup {
-        style = 'darker'
-      }
-      -- Enable theme
-      require('onedark').load()
+      vim.cmd([[colorscheme Tomorrow-Night-Bright]])
     end,
     enabled = true,
+  },
+  {
+    "armannikoyan/rusty",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme rusty]])
+    end,
+    enabled = false,
   }
 }
