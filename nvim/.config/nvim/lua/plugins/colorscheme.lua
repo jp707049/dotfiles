f35b4ff383a9e8f88a566aed8c04ade085285409
Jeet Patel <jeet_patel3@apple.com>
2026-02-10 23:16:27 -0600

    use onedarkpro

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
-  { src = "https://github.com/srcery-colors/srcery-vim" },
+  { src = "https://github.com/olimorris/onedarkpro.nvim" },
 })
 
-vim.cmd("colorscheme srcery")
+vim.cmd("colorscheme onedark_vivid")
 
