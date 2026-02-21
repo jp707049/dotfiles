0c55770285ca00f64fbae4e664b00c9f9bc861fd
Jeet Patel <jeet_patel3@apple.com>
2026-02-21 16:56:45 -0600

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,5 +2,9 @@ vim.pack.add({
   { src = "https://github.com/catppuccin/nvim" },
 })
 
+require("catppuccin").setup({
+  transparent_background = true,
+})
+
 vim.cmd("colorscheme catppuccin-mocha")
 
